e27a565bc1108f4f0f420d3062615b1a1d3addce
Best-spec <karami52468@gmail.com>
2025-07-25 10:20:36 +0700

    change path database for deploy

--- filemanager/settings.py
+++ filemanager/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,8 +32,7 @@ DEBUG = os.getenv("DEBUG") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['summary-inquiry.onrender.com', '127.0.0.1']
-
+ALLOWED_HOSTS = ['summary-inquiry.onrender.com','127.0.0.1']
 
 
 # Application definition
@@ -83,10 +83,13 @@ WSGI_APPLICATION = 'filemanager.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )    
 }
 
 
