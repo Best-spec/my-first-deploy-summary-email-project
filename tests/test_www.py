d07f242e99a3474cda783d03b46b2ad65c1e5683
Best-spec <karami52468@gmail.com>
2025-08-10 02:36:30 +0700

    ย้ายโค้ดทดสอบและปรับ pytest

--- tests/test_www.py
+++ tests/test_www.py
@@ -1,5 +1,6 @@
 import pytest
 from main.TopCenter.controllers.top_clinic_controller import find_top_clinics_summary
 
+
 def test_output():
-    assert isinstance(find_top_clinics_summary, list)
+    assert callable(find_top_clinics_summary)
