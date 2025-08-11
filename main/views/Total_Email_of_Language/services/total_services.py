3fdd4c250a48fabaa168b93e12868486da364173
Best-spec <karami52468@gmail.com>
2025-08-11 12:29:35 +0700

    fix compare func type by lang

--- main/views/Total_Email_of_Language/services/total_services.py
+++ main/views/Total_Email_of_Language/services/total_services.py
@@ -10,15 +10,16 @@ from ..models.chart6 import group_by_country_type
 from ..serializers.total_serializer import TotalSerializer
 
 
-def find_TotalMonth(date, web):
+
+def find_TotalMonth(date_ranges, web):
     try:
-        if len(date) <= 1:
+        if len(date_ranges) < 2:
             data_json = {}
             print("it 1")
-            summary, plot_data, transposed = cal_TotalMonth(date[0], web[0])
+            summary, plot_data, transposed = cal_TotalMonth(date_ranges[0], web[0])
             data_json['table'] = summary
             data_json['chart1'] = Grand_Total_By_Language(summary)
-            data_json['chart2'] = [cal_all_type_email(date[0])]
+            data_json['chart2'] = [cal_all_type_email(date_ranges[0])]
             data_json['chart3'] = Total_Email_Type_By_Language(summary)
             data_json['chart4'] = inquiry_by_lang(summary)
             data_json['chart5'] = appointment_by_lang(summary)
@@ -29,14 +30,23 @@ def find_TotalMonth(date, web):
             return json.data
         else :
             print("it 2")
-            totalset1, plot_data, transposed = cal_TotalMonth(date[0], web[0])
-            totalset2, plot_data, transposed = cal_TotalMonth(date[1], web[1])
-            type_email = cal_all_type_email(date[0])
-            compare = Resultcompare(totalset1, totalset2, date) 
-            return {
+            summary1, plot_data, transposed = cal_TotalMonth(date_ranges[0], web[0])
+            summary2, plot_data, transposed = cal_TotalMonth(date_ranges[1], web[1])
+            type_email = cal_all_type_email(date_ranges[0])
+            compare = Resultcompare(summary1, summary2, date_ranges) 
+            data_json = {
                 "table": compare,
-                "chart1": compare,
-                "chart2": type_email
+                "chart1": Grand_Total_By_Language(summary1),
+                "chart2": [cal_all_type_email(date_ranges[0])],
+                "chart3": Total_Email_Type_By_Language(summary1),
+                "chart4": inquiry_by_lang(summary1),
+                "chart5": appointment_by_lang(summary1),
+                "chart6": group_by_country_type(summary1),
+                "compare": Resultcompare(summary1, summary2, date_ranges),
             }
+
+            json = TotalSerializer(data_json)
+            return json.data
+        
     except Exception as e:
         print("error from cal total",e)
