79ec283d312ead51f7a454fec4fde845baec3617
Best-spec <Best-spec@users.noreply.github.com>
2026-08-21 01:46:23 +0700

    chore: remove obsolete local deployment and build scripts

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export DJANGO_SETTINGS_MODULE=main.settings.settings 
-echo "🔧 Running migrations..."
-python manage.py migrate --noinput
-
-echo "🚀 Starting server..."
-gunicorn main.settings.wsgi:application --bind 0.0.0.0:$PORT
